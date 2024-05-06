666cff7f837f84fceadbd873fbf24f1f1620f5a8
gagandeep.bhambrah <gagandeep.bhambrah@calsoftinc.com>
2024-05-06 18:55:44 +0530

    added test cases

--- src/tools/shortner.go
+++ src/tools/shortner.go
@@ -1,43 +1,44 @@
 package tools
 
-import (
-	"fmt"
-)
+import "fmt"
 
 const (
-	BASE = 62
+	BASE    = 62
+	CHARSET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 )
 
-func Shorten(longString string) (string, error) {
-	return "", nil
+var CHARSET_MAP = map[rune]int{
+	'0': 0, '1': 1, '2': 2, '3': 3, '4': 4,
+	'5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+	'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14,
+	'f': 15, 'g': 16, 'h': 17, 'i': 18, 'j': 19,
+	'k': 20, 'l': 21, 'm': 22, 'n': 23, 'o': 24,
+	'p': 25, 'q': 26, 'r': 27, 's': 28, 't': 29,
+	'u': 30, 'v': 31, 'w': 32, 'x': 33, 'y': 34,
+	'z': 35, 'A': 36, 'B': 37, 'C': 38, 'D': 39,
+	'E': 40, 'F': 41, 'G': 42, 'H': 43, 'I': 44,
+	'J': 45, 'K': 46, 'L': 47, 'M': 48, 'N': 49,
+	'O': 50, 'P': 51, 'Q': 52, 'R': 53, 'S': 54,
+	'T': 55, 'U': 56, 'V': 57, 'W': 58, 'X': 59,
+	'Y': 60, 'Z': 61,
 }
 
-func Expand(shortString string) (string, error) {
-	return "", nil
-}
-
-func getByteArray(data string) []byte {
-	bytes := make([]bytes, 0)
-	for _, r := range data {
-		bytes = append(bytes, r)
+func Shorten(id int) string {
+	var short string
+	for id > 0 {
+		short = string(CHARSET[id%BASE]) + short
+		id = id / BASE
 	}
-	return bytes
+	return short
 }
 
-func binStreamify(data []rune) string {
-	binStream := ""
-	for _, r := range data {
-		binStream += fmt.Sprintf("%b", r)
-	}
-	return binStream
-}
-
-func validCharset(data []rune) ([]rune, error) {
-	//check if the rune falls under 0-9, a-z, A-Z
-	for _, r := range data {
-		if !(r >= 48 && r <= 57) && !(r >= 65 && r <= 90) && !(r >= 97 && r <= 122) {
-			return nil, fmt.Errorf("Invalid character in the input string %s", string(r))
+func Expand(short string) (int, error) {
+	var id int
+	for _, c := range short {
+		if _, ok := CHARSET_MAP[c]; !ok {
+			return 0, fmt.Errorf("invalid character: %c", c)
 		}
+		id = id*BASE + CHARSET_MAP[c]
 	}
-	return data, nil
+	return id, nil
 }
---
+++ test/tools_test/shortener_test.go
@@ -0,0 +1,54 @@
+package tools_test
+
+import (
+	shortener "aiotools/src/tools"
+	"testing"
+)
+
+func TestShortenId(t *testing.T) {
+	const (
+		input = 123
+		want  = "1Z"
+	)
+	result := shortener.Shorten(123)
+	if result != want {
+		t.Errorf("Shorten(%d) = %s; want %s", input, result, want)
+	}
+}
+
+func TestExpandId_WhenProperInput_ThenProperOutput(t *testing.T) {
+	const (
+		input = "1Z"
+		want  = 123
+	)
+	result, e := shortener.Expand("1Z")
+	if e != nil {
+		t.Errorf("Expand(%s) = %d; want %d", input, result, want)
+	}
+	if result != want {
+		t.Errorf("Expand(%s) = %d; want %d", input, result, want)
+	}
+}
+
+func TestExpandId_WhenInvalidInput_ThenError(t *testing.T) {
+	const (
+		input = "1Z!"
+	)
+	_, e := shortener.Expand(input)
+	if e == nil {
+		t.Errorf("Expand(%s) = %v; want %v", input, e, "error")
+	}
+}
+
+func TestLongRandom(t *testing.T) {
+	for i := 0; i < 1000000; i++ {
+		short := shortener.Shorten(i)
+		result, e := shortener.Expand(short)
+		if e != nil {
+			t.Errorf("Expand(%s) = %d; want %d", short, result, i)
+		}
+		if result != i {
+			t.Errorf("Expand(%s) = %d; want %d", short, result, i)
+		}
+	}
+}
